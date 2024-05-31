66c71a6fa3f52f31b75737989cb3da9bbda0262b
eselene <eviougeas>
2024-05-31 11:06:32 +0200

    Ajout Filtre categorie dans l'index. Reur mefonte de methodes et ajout de controlleur main

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,12 +16,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/', name: 'app_article_index', methods: ['GET'])]
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(Request $request, ArticleRepository $articleRepository, CategorieRepository $categorieRepository): Response
     {
-
-
         return $this->render('article/index.html.twig', [
             'articles' => $articleRepository->findAll(),
+            // 'categories' => $categorieRepository->findAll(),
         ]);
     }
 
