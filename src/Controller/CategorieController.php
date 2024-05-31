b015f0f53022bf479ebf2b1f9d328b141580523b
eselene <eviougeas>
2024-05-31 11:50:28 +0200

    suite

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -71,7 +71,7 @@ class CategorieController extends AbstractController
     #[Route('/{id}', name: 'app_categorie_delete', methods: ['POST'])]
     public function delete(Request $request, Categorie $categorie, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$categorie->getId(), $request->getPayload()->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $categorie->getId(), $request->getPayload()->get('_token'))) {
             $entityManager->remove($categorie);
             $entityManager->flush();
         }
