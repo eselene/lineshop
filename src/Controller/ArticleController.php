67364627d40e29a2875e99a673b81fb00b88ba6b
lea-nova <lea.rulliere@outlook.fr>
2024-06-10 10:48:03 +0200

    debug route

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,16 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use App\Entity\User;
 use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
-use App\Repository\CategorieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -21,7 +18,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 // #[IsGranted('ROLE_EDIT')] // ArticleController est réservé pour le ROLE_EDIT
 class ArticleController extends AbstractController
 {
-    #[Route('editor/article', name: 'app_article_index', methods: ['GET'])]
+    #[Route('/', name: 'app_article_index', methods: ['GET'])]
     public function index(Request $request, ArticleRepository $articleRepository): Response
     {
 
@@ -32,7 +29,7 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/editor/new', name: 'app_article_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
 
         if ($this->denyAccessUnlessGranted("IS_AUTHENTICATED")) {
@@ -82,7 +79,7 @@ class ArticleController extends AbstractController
         ]);
     }
 
-    #[Route('editor/article/{id}', name: 'app_article_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_article_show', methods: ['GET'])]
     public function show(Article $article): Response
     {
         return $this->render('article/show.html.twig', [
